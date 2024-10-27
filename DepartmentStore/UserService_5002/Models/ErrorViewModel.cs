b39469170c99f5fc769463391401746acb023feb
NguyenQuys <nguyenhuuquy110203@gmail.com>
2024-10-27 09:48:39 +0700

    add 3 services

--- DepartmentStore/UserService_5002/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UserService_5002.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
