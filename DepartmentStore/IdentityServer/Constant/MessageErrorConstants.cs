1bd0fc33479b47c9c90572e8f9758f1b2894f079
NguyenQuys <nguyenhuuquy110203@gmail.com>
2024-11-29 12:00:03 +0700

    add doc

--- DepartmentStore/IdentityServer/Constant/MessageErrorConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IdentityServer.Constant
-{
-    public class MessageErrorConstants
-    {
-        public const string DO_NOT_FIND_DATA = "Không tìm thấy dữ liệu!";
-        public const string AUTHENTICATION_ERROR = "Bạn cần đăng nhập để thực hiện!";
-        public const string AUTHORIZATION_ERROR = "Bạn không có quyền thực hiện chức năng này!";
-    }
-}
